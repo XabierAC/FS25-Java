7385f577076c20df5a8e72c9930f8c03c0a69eb7
XabierAC <xabier139@gmail.com>
2025-01-07 23:42:01 +0100

    New Files .txt and mainMenu logic

--- FS25.java
+++ FS25.java
@@ -1,14 +1,45 @@
+import java.util.*;
+
 public class FS25 {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Campo 1:  Trigo");
-        System.out.println("5 - Cultivando");
-        System.out.println("Julio");
-        System.out.println();
-        System.out.println("Campo 2:  Colza");
-        System.out.println("2 - Arado");
-        System.out.println("Julio");
-        System.out.println();
-        Campo campo1 = new Campo(1, 2, "Trigo");
+        String selection = mainMenu();
+        if (selection.startsWith("C")) {
+            
+        } else if (selection.startsWith("V")) {
+            
+        }else {
+            System.out.println("Fin");
+        }
+    }
+
+    /* shows the main menu and takes the input from the keyboard */
+    public static String mainMenu() {
+        boolean correctOption = false;
+        String selectedOption = "";
+        Scanner inputScanner = new Scanner(System.in);
+        do {
+            System.out.println("Que quieres hacer?");
+            System.out.println("Editar los (C)ampos");
+            System.out.println("Editar los (V)ehículos");
+            System.out.println("(S)alir");
+            selectedOption = inputScanner.nextLine().toUpperCase();
+            if (selectedOption.startsWith("C") || selectedOption.startsWith("V") || selectedOption.startsWith("S")){
+                correctOption = true;
+            } else {
+                System.out.println("No has elegido una opcion valida, vuelve a intentarlo.");
+            }
+        } while (!correctOption);
+        inputScanner.close();
+        return selectedOption;
+    }
+
+    /* Shows the selectable menu to work with fields info */
+    public static void fieldMenu() {
+
+    }
+
+    /* Shows the selecable menu to work with vehicle info  */
+    public static void vehicleMenu() {
+
     }
 }
