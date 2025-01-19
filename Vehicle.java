f32ca83ce4d29544426c4890f799fd26e446ba48
XabierAC <xabier139@gmail.com>
2025-01-19 20:01:07 +0100

    getId method of class vehicle created

--- Vehicle.java
+++ Vehicle.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.util.*;
+
 public class Vehicle {
     private int id;
     private String brand;
@@ -13,5 +16,29 @@ public class Vehicle {
         this.horsepower = horsepower;
         this.damage = damage;
         this.plate = plate;
+    }
+    public static int getId(String inputString, Scanner inputScanner) throws FileNotFoundException {
+        File bddVehicle = new File("/Users/xabierac/Developer/Visual Studio Code/FS25 Java/bddVehicle.txt");
+        Scanner bddVehicleScanner = new Scanner(bddVehicle);
+        String brand = FS25.selectedText(inputString, 1);
+        String model = FS25.selectedText(inputString, 2);
+        int id = 0;
+        boolean found = false;
+        while (!found){
+            while (bddVehicleScanner.hasNextLine()) {
+                String textOfBdd = bddVehicleScanner.nextLine();
+                if (brand.equals(FS25.selectedText(textOfBdd, 2)) && model.equals(FS25.selectedText(textOfBdd, 3))) {
+                    found = true;
+                    id = Integer.parseInt(FS25.selectedText(textOfBdd, 1));
+                }
+            }
+            if (!found) {
+                System.out.println("El vehiculo introducido no existe en la base de datos.\nIntroduce una marca y modelo validos.");
+                brand = FS25.selectedText(inputString, 1);
+                model = FS25.selectedText(inputString, 2);
+            }
+        }
+        bddVehicleScanner.close();
+        return id;
     }
 }
